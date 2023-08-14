2050ccfea95b7b1c8911baf4eac48d05b46d8d61
Erdemeesgithub <berdemdelgerekh@gmail.com>
2023-08-14 14:01:19 +0800

    expand

--- components/CameraScreen.js
+++ components/CameraScreen.js
@@ -1,11 +1,40 @@
 import { Camera, CameraType } from "expo-camera";
 import { useRef, useState } from "react";
-import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Animated,
+} from "react-native";
 
 export function CameraScreen() {
   const [type, setType] = useState(CameraType.back);
   const [permission, requestPermission] = Camera.useCameraPermissions();
   const CameraRef = useRef();
+
+  const [ratioExpanded, setRatioExpanded] = useState(false);
+
+  const radioWidth = useRef(new Animated.Value(100)).current;
+
+  function expandWidth() {
+    setRatioExpanded(true);
+    Animated.timing(radioWidth, {
+      toValue: 200,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+  }
+
+  function shrinkWidth() {
+    setRatioExpanded(false);
+    Animated.timing(radioWidth, {
+      toValue: 100,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+  }
 
   if (!permission) {
     // Camera permissions are still loading
@@ -42,10 +71,28 @@ export function CameraScreen() {
           <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
             <Text style={styles.text}>Flip Camera</Text>
           </TouchableOpacity>
+
           <TouchableOpacity style={styles.button} onPress={takePic}>
             <Text style={styles.text}>take pic</Text>
           </TouchableOpacity>
         </View>
+        <TouchableOpacity
+          activeOpacity={0.8}
+          onPress={() => (ratioExpanded ? shrinkWidth() : expandWidth())}
+        >
+          <Animated.View
+            style={{
+              borderRadius: 20,
+              backgroundColor: "rgba(255, 255, 255, 0.4)",
+              width: radioWidth,
+              padding: 20,
+              margin: 20,
+              alignItems: "center",
+            }}
+          >
+            <Text style={{ color: "white" }}>4:3</Text>
+          </Animated.View>
+        </TouchableOpacity>
       </Camera>
     </View>
   );
